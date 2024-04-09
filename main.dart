0c8712513fce79bfe4959e9d1866f99bc567d8e0
Caren-Agala <carenagala324@gmail.com>
2024-04-09 17:26:23 +0300

    Plp week 2 assignment

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  int age = 24;
+  double weight = 77.5;
+  String name = "Jane Doe";
+
+  List<String> hobbies = ["hiking", "biking", "crotcheting"];
+  Map<String, int> scores = {'English': 77, 'Physics': 80, 'Maths': 76};
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Weight: $weight");
+  print("$name\'s hobbies are $hobbies");
+  print(("And their scores are $scores"));
+}
